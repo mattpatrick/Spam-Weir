563d2104d3b98ecf69e05a3220e461738a0ba006
mattpatrick <matthewleepatrick@gmail.com>
2013-11-25 11:16:35 -0500

    add response to webhooks code

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ var app = http.createServer(function(req, res) {
                 }
             else
                 {
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        res.end(index);
                 // var queryValueWebhooks = queryParsed.data;
                         var body = '';
                         req.on('data', function(data){
