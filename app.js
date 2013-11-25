b65882c4bfd2533072dfb2742f5ceab34ff6c792
mattpatrick <matthewleepatrick@gmail.com>
2013-11-25 11:24:45 -0500

    statuscod ->200

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ var app = http.createServer(function(req, res) {
                             });
                         }
  
-                          res.writeHead(statusCode, {'Content-Type': 'text/plain'});
+                          res.writeHead(200, {'Content-Type': 'text/plain'});
                           res.end();
 
                 }
