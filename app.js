e446ff9cf4f0b2c4c6f62c3311b19f33171612ea
mattpatrick <matthewleepatrick@gmail.com>
2013-11-24 23:58:26 -0500

    Webhooks copypasta

--- app.js
+++ app.js
@@ -32,9 +32,25 @@ var app = http.createServer(function(req, res) {
                 }
             else
                 {
-                    var header = url_parts.headers;
-                    var headersString = JSON.stringify(header);
-                console.log('Query string = ' + headersString);
+  
+                hook.on('callback', function (payload) {
+                // Triggered each time the webhook receives a payload.
+                // Here, we'll parse Venmo's signed response payload to
+                // get the sweet, sweet receipt API within.
+             
+                // Split and decode signature and data.
+                var parts = payload.payments.split('.');
+                var sig = new Buffer(parts[0], 'base64').toString('hex');
+                var data = JSON.parse(new Buffer(parts[1], 'base64').toString('utf8'))
+                // Validate its signature.
+                var expectedSig = crypto.createHmac('sha256', SECRET);
+                expectedSig.update(parts[1]);
+                expectedSig = expectedSig.digest('hex');
+                if (sig != expectedSig) throw new Error('Invalid signature!')
+ 
+    // Dump the JSON payload to console.
+    console.log(data);
+  });
                 }
 
             var urlString = url.format(url_parts);
