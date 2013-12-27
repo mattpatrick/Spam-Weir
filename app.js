f2c31d998c3d820dc45861cb238c94234bec75c7
mattpatrick <matthewleepatrick@gmail.com>
2013-12-26 21:00:07 -0500

    Webhooks to Parse

--- app.js
+++ app.js
@@ -102,8 +102,8 @@ var app = http.createServer(function(req, res) {
                                 dataParsed = dataJson.data;
                                 dataStringify = JSON.stringify(dataParsed);
                                 dataParsedJson = JSON.parse(dataString);
-                                webhooksId = dataParsedJson.id;
-                                webhooksStatus = dataParsedJson.status;
+                                webhooksId = dataParsed.id;
+                                webhooksStatus = dataParsed.status;
                                 updateStatus(webhooksId,webhooksStatus);
 
                                 console.log('Received body data:');
