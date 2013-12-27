9abe0e89dcadbdeeb7dc68e924d8f9bfbc09620e
mattpatrick <matthewleepatrick@gmail.com>
2013-12-26 21:50:50 -0500

    same

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ function updateTransactionStatus(transactionId, transactionStatus){
             query.first({
                 success: function(object) {
                         console.log('Webhooks update received');
-                        objectId = object.set('status',transactionStatus);
+                        objectId = object.set('Status',transactionStatus);
   
                    },
                 error: function(error) {
