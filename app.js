d38169f03b589d95875cb187f8fac3c37311996e
mattpatrick <matthewleepatrick@gmail.com>
2014-01-19 22:13:11 -0600

    Testing

--- app.js
+++ app.js
@@ -215,7 +215,7 @@ function venmoRequest(number){
     var request = require("request");
  
     request({
-      uri: "https://sandbox-api.venmo.com/v1",
+      uri: "https://sandbox-api.venmo.com/v1/payments",
       method: "POST",
       
       // form: {"access_token": "dymsdHqxz38vBueFznYaRzUzQtDdzK2H", "note" : "Test02", "amount" : "-1",  "phone" : number
